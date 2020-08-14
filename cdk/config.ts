a33952a092607868c3f270b38f2e0bc2da5ae403
superultra38 <36655462+superultra38@users.noreply.github.com>
2020-08-14 15:42:26 +0900

    Update config.ts

--- cdk/config.ts
+++ cdk/config.ts
@@ -2,6 +2,6 @@ export const githubOwner = 'peerjako-aws';
 export const repoName = 'cdk-ecs-cicd';
 export const gitDevBranch = 'dev'
 export const gitProdBranch = 'master'
-export const awsSecretsGitHubTokenName = 'github-personal-access-token';
+export const awsSecretsGitHubTokenName = '8b0e159dd7762766336241a21fced6b23658e23d';
 export const ssmImageTagParamName = 'penguinapp-imagetag';
 export const stagingValidationEmail = 'validation@test.com';
